57aee8a89a3cc1c66423ba351159131d185c56cb
Brian Kelley <mac@Brians-MacBook-Pro.local>
2023-01-04 08:27:40 -0600

    updated files

--- files/js/home.js
+++ files/js/home.js
@@ -1,10 +1,14 @@
+    let development = false;
+    let filePath = development ? 'files/' : 'home-22/files/';
+
+    const timeToCheck = 250;
 
     var animation = bodymovin.loadAnimation({
       container: document.getElementById('hero-animation'),
       renderer: 'svg',
       loop: true,
       autoplay: true,
-      path: 'files/json/hero.json'
+      path: `${filePath}json/hero.json`
     });
 
     // Carousel Support
@@ -26,15 +30,35 @@
       currentItem.classList.add('active');
     }
 
-    function removeHash () { 
+    function windowFuncs (e) { 
+      let target = e.currentTarget.getAttribute('href');
+      let url = `${window.location.origin}${target}`;
+
+      location.hash = target;
+
       setTimeout(function () {
         history.replaceState(null, null, ' ');
-      }, 100);
+      }, timeToCheck);
+    }
+
+    function checkOffset(e) {
+        const carouselViewport = document.querySelector('.carousel__viewport');
+
+        let target = e.currentTarget.getAttribute('href');
+        let targetElement = document.getElementById(target.split('#')[1]);
+
+        // Additional check if anchor fails
+        setTimeout(function () {
+          if (carouselViewport.scrollLeft != targetElement.offsetLeft) {
+            carouselViewport.scrollLeft = targetElement.offsetLeft;
+          }
+         }, timeToCheck);
     }
 
     carouselTriggers.forEach(trigger =>{
       trigger.addEventListener('click', (e)=> {
-        removeHash();
+        windowFuncs(e);
+        checkOffset(e);
         toggleClasses(e.currentTarget);
       })
     })
